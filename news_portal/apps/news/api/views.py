51dec2df50e4977100e78aed53739ee6fbe737f7
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-20 02:07:21 +0600

    TopHeadlineViewSet updated

--- news_portal/apps/news/api/views.py
+++ news_portal/apps/news/api/views.py
@@ -1,4 +1,5 @@
 import logging
+from wsgiref.util import request_uri
 from django.urls import reverse
 from rest_framework import viewsets, status, mixins
 from rest_framework.generics import get_object_or_404
@@ -20,12 +21,19 @@ class SourceViewSet(viewsets.ModelViewSet):
     pagination_class = pagination_global.GlobalPagination
 
 
-
 class TopHeadlineViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = models_news.TopHeadline.objects.all()
     serializer_class = serializers_news.TopHeadlineSerializer
     pagination_class = pagination_global.GlobalPagination
 
 
-
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            country_of_news = user.country_of_news
+            country_of_news = [s.lower() for s in country_of_news]
+            qs = self.queryset.filter(source__country__in=country_of_news, source__in=user.news_sources.all())
+            return qs
+        else:
+            return self.queryset
