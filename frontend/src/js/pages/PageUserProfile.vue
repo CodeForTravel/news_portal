fe460d931799699069b15537049c9c3bbf9fc023
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-20 01:00:28 +0600

    page user profile updated

--- frontend/src/js/pages/PageUserProfile.vue
+++ frontend/src/js/pages/PageUserProfile.vue
@@ -2,27 +2,35 @@
   <div class="my-4 container">
     <b-card no-body>
       <b-tabs pills card vertical>
-        <b-tab title="User Info" active><b-card-text>
-          <change-user-profile-form></change-user-profile-form>  
-        </b-card-text></b-tab>
-        <b-tab title="Change Password"><b-card-text>
-          <form-change-password></form-change-password>
-        </b-card-text></b-tab>
-        <b-tab title="Settings"><b-card-text>News Feed Settings</b-card-text></b-tab>
+        <b-tab title="User Info" active
+          ><b-card-text>
+            <change-user-profile-form></change-user-profile-form> </b-card-text
+        ></b-tab>
+        <b-tab title="Change Password"
+          ><b-card-text>
+            <form-change-password></form-change-password> </b-card-text
+        ></b-tab>
+        <b-tab title="Settings">
+          <b-card-text> <news-feed-settings></news-feed-settings> </b-card-text
+        ></b-tab>
       </b-tabs>
     </b-card>
   </div>
 </template>
 
 <script>
-const FormChangePassword =()=> import("../components/user/profile/FormChangePassword.vue")
-const ChangeUserProfileForm =()=> import("../components/user/profile/ChangeUserProfileForm.vue")
-
+const FormChangePassword = () =>
+  import("../components/user/profile/FormChangePassword.vue");
+const ChangeUserProfileForm = () =>
+  import("../components/user/profile/ChangeUserProfileForm.vue");
+const NewsFeedSettings = () =>
+  import("../components/user/profile/NewsFeedSettings.vue");
 
 export default {
   components: {
     ChangeUserProfileForm,
     FormChangePassword,
+    NewsFeedSettings,
   },
 };
 </script>
