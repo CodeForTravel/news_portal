6e188408b60e03f6810ad06220ae66650f87d938
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-20 02:06:38 +0600

    navbar updated

--- frontend/src/js/components/base/NavBar.vue
+++ frontend/src/js/components/base/NavBar.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand :to="{name:'home'}">News Portal</b-navbar-brand>
+      <b-navbar-brand :to="{ name: 'home' }">News Portal</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">News Feed</b-nav-item>
+          <b-nav-item
+            :active="$route.name == 'news-feed'"
+            :to="{ name: 'news-feed' }"
+            >News Feed</b-nav-item
+          >
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item v-if="!isAuthenticated" :to="{name:'login'}">Login</b-nav-item>
-          <b-nav-item v-if="isAuthenticated" :to="{name:'profile'}">User</b-nav-item>
+          <b-nav-item v-if="!isAuthenticated" :to="{ name: 'login' }"
+            >Login</b-nav-item
+          >
+          <b-nav-item
+            :active="$route.name == 'profile'"
+            v-if="isAuthenticated"
+            :to="{ name: 'profile' }"
+            >User</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -21,44 +32,36 @@
 </template>
 
 <script>
-import {  mapState } from "vuex";
+import { mapState } from "vuex";
 export default {
   data() {
-    return {
-
-    };
+    return {};
   },
 
-   computed: {
+  computed: {
     ...mapState(["user"]),
 
-    isAuthenticated(){
-      if(this.user.userInfo){
-        return true
+    isAuthenticated() {
+      if (this.user.userInfo) {
+        return true;
+      } else {
+        return false;
       }
-      else{
-        return false
-      }
-    }
+    },
   },
 
-  created(){
-    this.fetchUserInfo()
+  created() {
+    this.fetchUserInfo();
   },
 
   methods: {
     fetchUserInfo() {
-      this.$store
-        .dispatch("user/getUserInfo")
-        .then((resp) => {
-          console.log(resp.data)
-        });
+      this.$store.dispatch("user/getUserInfo").then((resp) => {
+        console.log(resp.data);
+      });
     },
   },
 };
 </script>
 
-
-<style>
-
-</style>
+<style></style>
