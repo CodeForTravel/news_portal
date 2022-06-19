e96d6b8a0ded712f9045229ff54a82ce56052e16
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-19 08:26:14 +0600

    navbar updated for authenticated user

--- frontend/src/js/components/base/NavBar.vue
+++ frontend/src/js/components/base/NavBar.vue
@@ -7,32 +7,13 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#">News Feed</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <b-nav-item v-if="!isAuthenticated" :to="{name:'login'}">Login</b-nav-item>
+        <b-nav-item v-if="isAuthenticated" href="#">User</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -40,10 +21,43 @@
 </template>
 
 <script>
+import {  mapState } from "vuex";
 export default {
+  data() {
+    return {
 
-}
+    };
+  },
+
+   computed: {
+    ...mapState(["user"]),
+
+    isAuthenticated(){
+      if(this.user.userInfo){
+        return true
+      }
+      else{
+        return false
+      }
+    }
+  },
+
+  created(){
+    this.fetchUserInfo()
+  },
+
+  methods: {
+    fetchUserInfo() {
+      this.$store
+        .dispatch("user/getUserInfo")
+        .then((resp) => {
+          console.log(resp.data)
+        });
+    },
+  },
+};
 </script>
+
 
 <style>
 
