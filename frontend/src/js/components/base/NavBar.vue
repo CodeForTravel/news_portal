3bf4d68d5f6f89a4969bf81ba16db8b47e227f03
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-19 08:43:49 +0600

    navbar updated

--- frontend/src/js/components/base/NavBar.vue
+++ frontend/src/js/components/base/NavBar.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">News Portal</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">News Portal</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">News Feed</b-nav-item>
-      </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">News Feed</b-nav-item>
+        </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item v-if="!isAuthenticated" :to="{name:'login'}">Login</b-nav-item>
-        <b-nav-item v-if="isAuthenticated" href="#">User</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="!isAuthenticated" :to="{name:'login'}">Login</b-nav-item>
+          <b-nav-item v-if="isAuthenticated" :to="{name:'profile'}">User</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
