572c9170301e25d58980dd3b2dc0b6b00ee3481b
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-19 17:49:05 +0600

    TopHeadlineSerializer ArticleSerializer created

--- news_portal/apps/news/api/serializers.py
+++ news_portal/apps/news/api/serializers.py
@@ -1,20 +1,56 @@
 from rest_framework import serializers
 from news_portal.apps.news import models as models_news
-from news_portal.core.api.serializers import DynamicFieldsModelSerializer
 
 
 
-class SourceSerializer(DynamicFieldsModelSerializer):
+class SourceSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
     class Meta:
         model = models_news.Source
         fields = [
             "id",
             "name",
-            "source_id"
-            "description"
-            "url"
-            "category"
-            "language"
+            "source_id",
+            "description",
+            "url",
+            "category",
+            "language",
             "country"
+        ]
+
+
+class TopHeadlineSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    class Meta:
+        model = models_news.TopHeadline
+        fields = [
+            "id",
+            "source",
+            "headline_id",
+            "name",
+            "author",
+            "title",
+            "description",
+            "url",
+             "urlToImage",
+            "publishedAt",
+            "content"
+        ]
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    class Meta:
+        model = models_news.Article
+        fields = [
+            "id",
+            "source",
+            "author",
+            "title",
+            "description",
+            "url",
+            "urlToImage",
+            "publishedAt",
+            "content",
+
         ]
