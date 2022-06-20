c0487528a0543269de198b42b0f33ca5a35af31c
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-20 12:51:32 +0600

    navbar updated

--- frontend/src/js/components/base/NavBar.vue
+++ frontend/src/js/components/base/NavBar.vue
@@ -16,15 +16,16 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item v-if="!isAuthenticated" :to="{ name: 'login' }"
-            >Login</b-nav-item
-          >
+          <b-nav-item v-if="!isAuthenticated" :to="{ name: 'login' }">
+            Login
+          </b-nav-item>
           <b-nav-item
             :active="$route.name == 'profile'"
             v-if="isAuthenticated"
             :to="{ name: 'profile' }"
-            >User</b-nav-item
           >
+            <i class="far fa-user"></i>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
