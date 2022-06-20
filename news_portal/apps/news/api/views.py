d1df481119120c971ea69339b4cc84361b8f9a85
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-06-20 18:16:43 +0600

    wip

--- news_portal/apps/news/api/views.py
+++ news_portal/apps/news/api/views.py
@@ -1,10 +1,5 @@
 import logging
-from wsgiref.util import request_uri
-from django.urls import reverse
-from rest_framework import viewsets, status, mixins
-from rest_framework.generics import get_object_or_404
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from news_portal.apps.news import models as models_news
 from news_portal.apps.news.api import serializers as serializers_news
